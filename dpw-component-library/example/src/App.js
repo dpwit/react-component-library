8d77888a1c1fca4190a7d88d1a48ba5670eb61ca
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-11 09:01:27 +0000

    Updated project

--- dpw-component-library/example/src/App.js
+++ dpw-component-library/example/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import { ExampleComponent } from 'dpw-component-library'
+import { Title } from 'dpw-component-library/src'
 import 'dpw-component-library/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="Create React Library Example" />;
+  return <Title text="Title component" />
 }
 
 export default App
+
