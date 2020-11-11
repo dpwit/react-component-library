2120f6e44a96f4e9d12d776ed3ca44bcd2710565
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-11 09:34:46 +0000

    Added Button component

--- dpw-component-library/example/src/App.js
+++ dpw-component-library/example/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import { ExampleComponent } from 'dpw-component-library'
-import { Title } from 'dpw-component-library/src'
+//import Title from './Title'
 import 'dpw-component-library/dist/index.css'
 
 const App = () => {
   return <ExampleComponent text="Create React Library Example" />;
-  return <Title text="Title component" />
+  //return <Title text="Title component" />
 }
 
 export default App
